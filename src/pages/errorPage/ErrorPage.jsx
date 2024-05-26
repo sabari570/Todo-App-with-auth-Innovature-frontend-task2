a803474191e7bf2ddd01cd2cc20c2ae362ab8225
sabari570 <rsabarisai2000@gmail.com>
2024-05-26 13:36:27 +0530

    modified the error page designs

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -13,7 +13,8 @@ const ErrorPage = () => {
   return (
     <div className="error-page">
       <div className="error-container">
-        <h1 className="error-title">404 - Page Not Found</h1>
+        <h1 className="error-title">404</h1>
+        <h4>Opps! Page Not Found</h4>
         <p className="error-message">
           Sorry, the page you are looking for does not exist.
         </p>
